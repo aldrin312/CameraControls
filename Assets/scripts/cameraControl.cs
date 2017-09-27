b561ad7c8eeb7063c60e4430219501b00af30145
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-09-27 10:37:09 -0400

    fist commit
    fist commit

---
+++ Assets/scripts/cameraControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+	private Camera cam;
+	Rigidbody2D rBody;
+	private bool inBackGround1;
+	private bool inBackGround2;
+	private bool inBackGround3;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		rBody = this.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	private void FixedUpdate()
+	{
+		if (inBackGround1 == true)
+		{
+			cam.transform.position = new Vector3(this.transform.position.x, 0, -1);
+			cam.orthographicSize = 0.5f;
+
+		}
+		else if (inBackGround2 == true)
+		{
+			cam.transform.position = new Vector3(1.65f, 0, -1);
+			cam.orthographicSize = 2f;
+		}
+		else if (inBackGround3 == true)
+		{
+			cam.transform.position = new Vector3(this.transform.position.x, 0, -1);
+			if (rBody.velocity.x > 0)
+			{
+				cam.orthographicSize -= (cam.orthographicSize * 0.02f);
+			}
+			else if (rBody.velocity.x  <0)
+			{
+				cam.orthographicSize += (cam.orthographicSize * 0.02f);
+			}
+
+			}
+
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "1")
+		{
+			inBackGround1 = true;
+			inBackGround2 = false;
+			inBackGround3 = false;
+
+		}
+		else if(other.tag == "2")
+		{
+			inBackGround1 = false;
+			inBackGround2 = true;
+			inBackGround3 = false;
+		}
+		else if(other.tag == "3")
+		{
+			inBackGround1 = false;
+			inBackGround2 = false;
+			inBackGround3 = true;
+		}
+
+	}
+}
---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+	public float speed;
+
+	Rigidbody2D rBody;
+	// Use this for initialization
+	void Start () {
+		rBody = this.GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector3 movement = new Vector2(moveHorizontal, 0);
+		rBody.velocity = movement * speed;
+	}
+}
